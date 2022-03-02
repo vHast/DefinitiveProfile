100d5f12e3d897e24f8a782868b246c29be54511
Federico Alvarez <fedvhast@gmail.com>
2022-03-02 01:01:42 +0100

    Google Maps integration

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ document.querySelectorAll('.nav-link').forEach(n => n.addEventListener("click", 
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }))
+
+// Map -- Contact.html
+
+// Initialize and add the map
+function initMap() {
+  // The location of Barcelona
+  const barcelona = { lat: 41.3874, lng: 2.1686 };
+  // The map, centered at Barcelona
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 6,
+    center: barcelona,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: barcelona,
+    map: map,
+  });
+}
