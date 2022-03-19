6e64c288e6df62cafe3f50585f9098f039f67878
vHast <fedvhast@gmail.com>
2022-03-19 12:02:13 +0100

    Translate DOM init

--- fading.js
+++ fading.js
@@ -3,3 +3,21 @@
 let fadingProj = document.getElementById('projectsignal');
 fadingProj.style.display = 'flex';
 
+
+// Translation
+
+let basedLocation = document.getElementById('based');
+let basedSubtitles = document.getElementById('based-sub');
+let stackTxt = document.getElementById('stacktext');
+
+const translationEE = () => {
+  basedLocation.innerHTML = "Asub Tallinnas, Eestis";
+  basedSubtitles.innerHTML = "Kujundan ja arendan responsiivseid, optimeeritud ja tõhusaid veebisaite, vabakutseliste võimaluste jaoks saadaval.";
+  stackTxt.innerHTML = "Minu Stack";
+}
+
+const translationES = () => {
+  basedLocation.innerHTML = "Based in Tallinn, Estonia";
+  basedSubtitles.innerHTML = "I design and develop responsive, optimized and efficient websites, available for freelance opportunities.";
+  stackTxt.innerHTML = "My Stack";
+}
